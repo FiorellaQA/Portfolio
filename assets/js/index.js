1266ee3a5fc2c9bc86aa598026bb6c5beae47b7d
FiorellaQA <fiorella_90_17@hotmail.com>
2017-09-01 14:45:48 -0500

    añadiendo proyectos

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,38 +1,6 @@
 'use strict';
 
-// function render(root) {
-// 	root.empty();
-//
-// 	if(state.screen == null) {
-// 		root.append(Home(function () {
-// 			render(root);
-// 		}))
-// 	}else if(state.screen == "about"){
-// 		root.append(About(function () {
-// 			render(root);
-// 		}));
-// 	}else if(state.screen == "projects"){
-// 		root.append(Projects(function () {
-// 			render(root);
-// 		}));
-// 	}
-// }
-//
-// var state = {
-// 	screen: null
-// };
-
 $(function() {
-    // var myVideo=document.getElementById("video1");
-    // $("#video1").on("click",playPause());
-    //     function playPause() {
-    //         if (myVideo.paused){
-    //             myVideo.play();
-		// 	} else{
-    //             myVideo.pause();
-		// 	}
-    //
-    //     }
 
     function collapseNavbar() {
         if ($(".home__nav--js").offset().top > 50) {
@@ -45,61 +13,26 @@ $(function() {
 
 
 
-		// var root =$('#root');
-		// render(root);
-
-
-	$(document).on("click", ".close_button", function () {
-		$(this).closest(".grown").toggleClass("spot grown clickable");
-	});
-
-	$(document).on("click", ".menu", function () {
-		$('.home__nav--js').toggleClass("open menu");
+	$('a#toggle').on("click", function (e) {
+		$('nav').toggleClass("open menu");
+        e.preventDefault();
 	});
 
 
-    //
-	// $(window).scroll(function () {
-	// 	var sc = $(window).scrollTop()
-	// 	if (sc > 100) {
-	// 		$("#navbar-main").addClass("navbar-small")
-	// 	} else {
-	// 		$("#navbar-main").removeClass("navbar-small")
-	// 	}
-	// });
-
-
-	// if($(document).scrollTop() > 260 ) {
-	// 	if(transparent) {
-	// 		transparent = false;
-	// 		$('.navbar-color-on-scroll').removeClass('navbar-transparent');
-	// 	}
-	// } else {
-	// 	if( !transparent ) {
-	// 		transparent = true;
-	// 		$('.navbar-color-on-scroll').addClass('navbar-transparent');
-	// 	}
-	// }
-
-	$('#toggle').on('click',function (event) {
-		event.preventDefault();
-		$('#nav-header').toogleClass('open');
-		 $('#body').classList.toggle("overflow-hidden");
-
-	});
-    //
-	// $('#more-js').on('click', function () {
-	// 	header.addClass('page-moveToLeftEasing');
-    //
-	// 	setTimeout(function () {
-	// 		update();
-	// 	},1000);
-	// });
-	// $('#project-js').on('click', function () {
-	// 	header.addClass('page-moveToRightEasing');
-	// 	setTimeout(function () {
-	// 		update();
-	// 	},1000);
-	// });
 
 });
+
+//
+// $('#more-js').on('click', function () {
+// 	header.addClass('page-moveToLeftEasing');
+//
+// 	setTimeout(function () {
+// 		update();
+// 	},1000);
+// });
+// $('#project-js').on('click', function () {
+// 	header.addClass('page-moveToRightEasing');
+// 	setTimeout(function () {
+// 		update();
+// 	},1000);
+// });
